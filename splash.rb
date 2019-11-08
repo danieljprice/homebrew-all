4cb399773069d7e8ff09001281e83dc10094d2eb
Daniel Price <daniel.price@monash.edu>
2019-11-08 14:56:43 +1100

    brew version installs hdf5 readers also

--- splash.rb
+++ splash.rb
@@ -6,10 +6,11 @@ class Splash < Formula
   sha256 "212bbe16c56d1a009acbd263689ef6a30052d5bb4878aad345d97927fbdd1996"
   depends_on "gcc"
   depends_on "giza"
+  depends_on "hdf5"
 
   def install
     ENV.deparallelize
-    system "make", "SYSTEM=gfortran", "PREFIX=#{prefix}/", "GIZA_DIR=#{HOMEBREW_PREFIX}", "CC=#{ENV.cc}"
+    system "make", "all", "gadgethdf5", "falcON", "amuse_hdf5", "cactus_hdf5", "SYSTEM=gfortran", "PREFIX=#{prefix}/", "GIZA_DIR=#{HOMEBREW_PREFIX}", "CC=#{ENV.cc}", "HDF5_DIR=#{HOMEBREW_PREFIX}"
     mkdir_p bin.to_s # equivalent to mkdir -p #{bin}
     system "make", "install", "PREFIX=#{prefix}"
   end
