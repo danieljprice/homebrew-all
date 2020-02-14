b0df80bb72da9f04c6b50931a09aa453bf2ebaf1
Daniel Price <daniel.price@monash.edu>
2020-02-14 18:03:09 +1100

    updated to 2.10.0

--- splash.rb
+++ splash.rb
@@ -2,8 +2,8 @@
 class Splash < Formula
   desc "Smoothed Particle Hydrodynamics visualisation tool"
   homepage "http://users.monash.edu.au/~dprice/splash"
-  url "http://users.monash.edu.au/~dprice/splash/releases/splash-2.9.1.tar.gz"
-  sha256 "212bbe16c56d1a009acbd263689ef6a30052d5bb4878aad345d97927fbdd1996"
+  url "http://users.monash.edu.au/~dprice/splash/releases/splash-2.10.0.tar.gz"
+  sha256 "a5ca28e0c2f2060d2bc5443f4960dec36d0e7abf893ef3c192e3b764e4c98178"
   depends_on "gcc"
   depends_on "giza"
   depends_on "hdf5"
