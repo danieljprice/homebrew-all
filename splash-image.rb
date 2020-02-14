bae0d8e126601030c9767c1a3279315a4e1b02b9
Daniel Price <daniel.price@monash.edu>
2020-02-14 15:53:48 +1100

    added tap for splash-image

---
+++ splash-image.rb
@@ -0,0 +1,23 @@
+# splash homebrew install script
+class Splash < Formula
+  desc "Smoothed Particle Hydrodynamics image manipulation tools"
+  homepage "http://users.monash.edu.au/~dprice/splash"
+  url "https://github.com/danieljprice/splash", :using => :git
+  depends_on "gcc"
+  depends_on "cfitsio"
+
+  #head do
+  #  url "https://github.com/danieljprice/splash", :using => :git
+  #end
+
+  def install
+    ENV.deparallelize
+    system "make", "splash-image", "SYSTEM=gfortran", "PREFIX=#{prefix}/", "FITS_DIR=#{HOMEBREW_PREFIX}"
+    mkdir_p bin.to_s # equivalent to mkdir -p #{bin}
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    system "false"
+  end
+end
