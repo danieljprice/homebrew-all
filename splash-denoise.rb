56383ecf2e774431d862651f03d800c1217425ca
Daniel Price <daniel.price@monash.edu>
2020-02-17 09:43:30 +1100

    added splash-denoise formula

--- splash-denoise.rb
+++ splash-denoise.rb
@@ -1,12 +1,10 @@
 # splash homebrew install script
-class Splash < Formula
+class SplashDenoise < Formula
   desc "Smoothed Particle Hydrodynamics visualisation tool"
   homepage "http://users.monash.edu.au/~dprice/splash"
   url "http://users.monash.edu.au/~dprice/splash/releases/splash-2.10.0.tar.gz"
   sha256 "a5ca28e0c2f2060d2bc5443f4960dec36d0e7abf893ef3c192e3b764e4c98178"
   depends_on "gcc"
-  depends_on "giza"
-  depends_on "hdf5"
   depends_on "cfitsio"
 
   head do
@@ -15,7 +13,7 @@ class Splash < Formula
 
   def install
     ENV.deparallelize
-    system "make", "all", "gadgethdf5", "falcON", "amuse_hdf5", "cactus_hdf5", "fits", "SYSTEM=gfortran", "PREFIX=#{prefix}/", "GIZA_DIR=#{HOMEBREW_PREFIX}", "CC=#{ENV.cc}", "HDF5ROOT=#{HOMEBREW_PREFIX}"
+    system "make", "splash-denoise", "SYSTEM=gfortran", "PREFIX=#{prefix}/", "CC=#{ENV.cc}", "FITS_DIR=#{HOMEBREW_PREFIX}"
     mkdir_p bin.to_s # equivalent to mkdir -p #{bin}
     system "make", "install", "PREFIX=#{prefix}"
   end
