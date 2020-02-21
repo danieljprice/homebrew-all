b0deea6f7c0e48ca7a5f845ffe06f223fe23b6dd
Daniel Price <daniel.price@monash.edu>
2020-02-21 11:21:51 +1100

    splash-denoise->denoise

--- denoise.rb
+++ denoise.rb
@@ -1,6 +1,6 @@
 # splash homebrew install script
-class SplashDenoise < Formula
-  desc "Smoothed Particle Hydrodynamics visualisation tool"
+class Denoise < Formula
+  desc "Utility for de-noising astronomical images using adaptive kernel interpolation"
   homepage "http://users.monash.edu.au/~dprice/splash"
   url "http://users.monash.edu.au/~dprice/splash/releases/splash-2.10.0.tar.gz"
   sha256 "a5ca28e0c2f2060d2bc5443f4960dec36d0e7abf893ef3c192e3b764e4c98178"
@@ -13,7 +13,7 @@ class SplashDenoise < Formula
 
   def install
     ENV.deparallelize
-    system "make", "splash-denoise", "SYSTEM=gfortran", "PREFIX=#{prefix}/", "CC=#{ENV.cc}", "FITS_DIR=#{HOMEBREW_PREFIX}"
+    system "make", "denoise", "SYSTEM=gfortran", "PREFIX=#{prefix}/", "CC=#{ENV.cc}", "FITS_DIR=#{HOMEBREW_PREFIX}"
     mkdir_p bin.to_s # equivalent to mkdir -p #{bin}
     system "make", "install", "PREFIX=#{prefix}"
   end
