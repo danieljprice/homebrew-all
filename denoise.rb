52f55a17054e079f90f9c44b2f0f2fc7bfa8b904
Daniel Price <daniel.price@monash.edu>
2020-08-26 23:34:48 +1000

    (denoise) build from denoise repo, not from splash repo

--- denoise.rb
+++ denoise.rb
@@ -1,19 +1,19 @@
 # splash homebrew install script
 class Denoise < Formula
   desc "Utility for de-noising astronomical images using adaptive kernel interpolation"
-  homepage "http://users.monash.edu.au/~dprice/splash"
-  url "http://users.monash.edu.au/~dprice/splash/releases/splash-2.10.0.tar.gz"
-  sha256 "a5ca28e0c2f2060d2bc5443f4960dec36d0e7abf893ef3c192e3b764e4c98178"
+  homepage "http://github.com/danieljprice/denoise"
+  url "https://github.com/danieljprice/denoise/releases/download/v0.1.4/denoise-v0.1.4.tar.gz"
+  sha256 "705f4a6672784ab65e30eb0b34d89f120128f55ddcf0842c2300ef0afd763b64"
   depends_on "gcc"
   depends_on "cfitsio"
 
   head do
-    url "https://github.com/danieljprice/splash", :using => :git
+    url "https://github.com/danieljprice/denoise", :using => :git
   end
 
   def install
     ENV.deparallelize
-    system "make", "denoise", "SYSTEM=gfortran", "PREFIX=#{prefix}/", "CC=#{ENV.cc}", "FITS_DIR=#{HOMEBREW_PREFIX}"
+    system "make", "denoise", "SYSTEM=gfortran", "PREFIX=#{prefix}/", "FITS_DIR=#{HOMEBREW_PREFIX}", "DOUBLEPRECISION=no"
     mkdir_p bin.to_s # equivalent to mkdir -p #{bin}
     system "make", "install", "PREFIX=#{prefix}"
   end
