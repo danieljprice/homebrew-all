d97455ab8ee61bc5e16373b92b1549ae8870f39f
Daniel Price <daniel.price@monash.edu>
2021-01-15 13:04:00 +1100

    fix deprecated :x11 in cairo build; replace with specific x11 libraries needed

--- cairo-x11.rb
+++ cairo-x11.rb
@@ -3,6 +3,7 @@ class CairoX11 < Formula
   homepage "https://cairographics.org/"
   url "https://cairographics.org/releases/cairo-1.16.0.tar.xz"
   sha256 "5e7b29b3f113ef870d1e3ecf8adf21f923396401604bda16d44be45e66052331"
+  revision 2
 
   head do
     url "https://anongit.freedesktop.org/git/cairo", :using => :git
@@ -18,7 +19,13 @@ class CairoX11 < Formula
   depends_on "libpng"
   depends_on "lzo"
   depends_on "pixman"
-  depends_on :x11
+  # depends_on :x11
+  depends_on "libx11"
+  depends_on "libxcb"
+  depends_on "libxt"
+  depends_on "libxext"
+  depends_on "libxrender"
+
   conflicts_with "cairo", :because => "homebrew version does not contain X-windows driver"
 
   def install
