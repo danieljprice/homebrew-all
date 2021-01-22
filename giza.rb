8880c831784de9fb7350d4ebc4a0a71f4c6407ce
Daniel Price <daniel.price@monash.edu>
2021-01-22 16:21:54 +1100

    (giza) update x11 dependency

--- giza.rb
+++ giza.rb
@@ -7,7 +7,8 @@ class Giza < Formula
   sha256 "607c557fa5e4fadb7ebeab9e5459c8591e461552972e08722e602f4b4280372d"
   depends_on "gcc" => :build
   depends_on "cairo-x11" => :build
-  depends_on :x11
+  #depends_on :x11
+  depends_on "libx11"
 
   head do
     url "https://github.com/danieljprice/giza", :using => :git
