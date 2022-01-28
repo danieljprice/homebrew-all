f895fcd488dc0f48be5cde4ffd0255c31390b751
Daniel Price <daniel.price@monash.edu>
2022-01-28 04:57:32 +0000

    giza-x11 1.3.1

--- giza-x11.rb
+++ giza-x11.rb
@@ -2,9 +2,9 @@
 class GizaX11 < Formula
   desc "Scientific plotting library for C/Fortran"
   homepage "https://danieljprice.github.io/giza/"
-  url "https://github.com/danieljprice/giza/releases/download/v1.3.0/giza-v1.3.0.tar.gz"
-  version "1.3.0"
-  sha256 "a38f746d9a5349e013aebdaa09f2ddfe636b740348c5d89aec30382c2fe078f9"
+  url "https://github.com/danieljprice/giza/releases/download/v1.3.1/giza-v1.3.1.tar.gz"
+  version "1.3.1"
+  sha256 "c81d133db33e3134fc9b9b7bf93bff2bfaf7ed2b3f968a9125588647e2bf46c7"
   depends_on "gcc" => :build
   depends_on "cairo" => :build
   depends_on "libx11"
