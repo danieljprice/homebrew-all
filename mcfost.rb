39bd64c3c5f3f9af701134240846c1df1db43088
Daniel Price <daniel.price@monash.edu>
2023-01-12 15:59:54 +1100

    (mcfost) fix brew audit issues; head points to danieljprice fork

--- mcfost.rb
+++ mcfost.rb
@@ -2,29 +2,29 @@
 class Mcfost < Formula
   desc "Monte Carlo Radiative Transfer code"
   homepage "https://ipag.osug.fr/~pintec/mcfost/docs/html/index.html"
-  url "https://github.com/cpinte/mcfost", :using => :git
+  url "https://github.com/cpinte/mcfost.git"
   version "4.0"
+
+  # specifying --HEAD installs from danieljprice fork (hack)
+  head "https://github.com/danieljprice/mcfost.git"
+
+  depends_on "cfitsio"
   depends_on "gcc"
   depends_on "hdf5"
-  depends_on "cfitsio"
   depends_on "sprng2"
   depends_on "voro-dev"
 
-  head do
-    url "https://github.com/cpinte/mcfost", :using => :git, branch: "AtomicTransfer"
-  end
-
   def install
     ENV.deparallelize
-    Dir.chdir('src/')
+    Dir.chdir("src/")
     mkdir_p lib.to_s # equivalent to mkdir -p #{lib}
     mkdir_p include.to_s
-    system "make","all","SYSTEM=gfortran", "MCFOST_INSTALL=#{HOMEBREW_PREFIX}", \
-           "MCFOST_NO_XGBOOST=yes","MCFOST_GIT=1","CXX=#{ENV.cxx}", \
-           "INCLUDE=-I#{HOMEBREW_PREFIX}/include","LIBS=#{HOMEBREW_PREFIX}/lib"
-    bin.install Dir["mcfost"]
-    lib.install Dir["libmcfost.a"]
-    include.install Dir["mcfost2phantom.mod"]
+    system "make", "all", "SYSTEM=gfortran", "MCFOST_INSTALL=#{HOMEBREW_PREFIX}", \
+           "MCFOST_NO_XGBOOST=yes", "MCFOST_GIT=1", "CXX=#{ENV.cxx}", \
+           "INCLUDE=-I#{HOMEBREW_PREFIX}/include", "LIBS=#{HOMEBREW_PREFIX}/lib"
+    bin.install "mcfost"
+    lib.install "libmcfost.a"
+    include.install "mcfost2phantom.mod"
   end
 
   test do
