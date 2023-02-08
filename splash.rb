763f055302b8afb6189536df3db607a6ddde110e
Daniel Price <daniel.price@monash.edu>
2023-02-08 22:09:58 +0000

    splash 3.7.0

--- splash.rb
+++ splash.rb
@@ -2,8 +2,9 @@
 class Splash < Formula
   desc "Smoothed Particle Hydrodynamics visualisation tool"
   homepage "http://users.monash.edu.au/~dprice/splash"
-  url "https://github.com/danieljprice/splash/releases/download/v3.5.1/splash-v3.5.1.tar.gz"
-  sha256 "9bcac830d6810efa3ee4360593b37d4180773b685a528b58fd0331ce8546288a"
+  url "https://github.com/danieljprice/splash/releases/download/v3.7.0/splash-v3.7.0.tar.gz"
+  version "3.7.0"
+  sha256 "152978e84d4518b2a2dfb9933d82d9b000dac638201719a14527b5237ac09fc4"
 
   head "https://github.com/danieljprice/splash.git"
 
