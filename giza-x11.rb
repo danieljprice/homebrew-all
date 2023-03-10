9e209fdd6fea6a17620f1746c93f651e5ee70352
Daniel Price <daniel.price@monash.edu>
2023-03-10 13:24:01 +1100

    updated giza to depend on pkg-config

--- giza-x11.rb
+++ giza-x11.rb
@@ -5,6 +5,7 @@ class GizaX11 < Formula
   url "https://github.com/danieljprice/giza/releases/download/v1.3.2/giza-v1.3.2.tar.gz"
   version "1.3.2"
   sha256 "87f14679923ba729a13bc18026178be684d7620aaf0a5eb4172a4a1c9c87c033"
+  depends_on "pkg-config" => :build
   depends_on "gcc" => :build
   depends_on "cairo" => :build
   depends_on "libx11"
