cd1b22be7c57927cbd01c29b41c7f59d9db4c22a
Daniel Price <daniel.price@monash.edu>
2025-04-09 11:45:49 +1000

    finufft: implement brew test equivalent to make test

--- finufft.rb
+++ finufft.rb
@@ -12,15 +12,69 @@ class Finufft < Formula
 
   def install
     system "cmake", "-S", ".", "-B", "build",
-           "-DFINUFFT_BUILD_TESTS=OFF",
+           "-DFINUFFT_BUILD_TESTS=ON",
+           "-DFINUFFT_BUILD_EXAMPLES=ON",
+           "-DFINUFFT_BUILD_FORTRAN=ON",
            "-DCMAKE_INSTALL_PREFIX=#{prefix}",
            "-DCMAKE_C_COMPILER=#{ENV.cc}",
-           "-DCMAKE_CXX_COMPILER=#{ENV.cxx}"
+           "-DCMAKE_CXX_COMPILER=#{ENV.cxx}",
+           "-DCMAKE_CXX_FLAGS='-std=c++17'"
     system "cmake", "--build", "build"
+
+    # Install test files to share directory after build but before install
+    (share/"finufft").install "test"
+
     system "cmake", "--install", "build"
+
+    # Ensure all headers are installed
+    include.install Dir["include/finufft.h"]
+    (include/"finufft").install Dir["include/finufft/*"]
   end
 
   test do
-    system "#{bin}/finufft_test"
+    # Write a minimal test program
+    (testpath/"test.cpp").write <<~EOS
+      #include <finufft.h>
+      int main() {
+        finufft_opts opts;
+        finufft_default_opts(&opts);
+        return 0;
+      }
+    EOS
+
+    system ENV.cxx, "test.cpp", "-std=c++17",
+           "-I#{include}", "-L#{lib}", "-lfinufft",
+           "-o", "test"
+    system "./test"
+
+    # Now reproduce the make test functionality
+    # Copy test directory to testpath and set up results directory
+    cp_r "#{HOMEBREW_PREFIX}/share/finufft/test/.", testpath
+    (testpath/"results").mkpath
+
+    # Find and compile all test programs
+    Dir["#{HOMEBREW_PREFIX}/share/finufft/test/*.cpp"].each do |src|
+      basename = File.basename(src, ".cpp")
+      # Double precision version
+      system ENV.cxx, "-std=c++17", "-I#{include}",
+             src, "-L#{HOMEBREW_PREFIX}/lib",
+             "-lfinufft", "-lfftw3",
+             "-o", basename
+
+      # Single precision version
+      system ENV.cxx, "-std=c++17", "-DSINGLE", "-I#{include}",
+             src, "-L#{HOMEBREW_PREFIX}/lib",
+             "-lfinufft", "-lfftw3f",
+             "-o", "#{basename}f"
+    end
+
+    # Run the tests
+    ENV["OMP_NUM_THREADS"] = "4"
+    system "./basicpassfail"
+    system "./basicpassfailf"
+
+    # Run the shell script tests
+    system "./check_finufft.sh"
+    system "./check_finufft.sh", "SINGLE"
   end
-end 
+end
