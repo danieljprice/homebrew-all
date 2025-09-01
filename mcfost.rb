03b42efbd12a69350b20f8ab5056cd4748b85e1f
Daniel Price <daniel.price@monash.edu>
2025-09-01 12:14:20 +0200

    (mcfost) add test target for checking mcfost installed correctly; fix other warnings

--- mcfost.rb
+++ mcfost.rb
@@ -3,9 +3,9 @@ class Mcfost < Formula
   desc "Monte Carlo Radiative Transfer code"
   homepage "https://ipag.osug.fr/~pintec/mcfost/docs/html/index.html"
   url "https://github.com/cpinte/mcfost.git", :branch => "main"
-  version "4.1.03"
+  version "4.1.10"
 
-  # specifying --HEAD installs from danieljprice fork (hack)
+  # specifying --HEAD installs from DJP's fork instead
   head "https://github.com/danieljprice/mcfost.git", :branch => "main"
 
   depends_on "cfitsio"
@@ -16,18 +16,43 @@ class Mcfost < Formula
 
   def install
     ENV.deparallelize
-    Dir.chdir("src/")
-    mkdir_p lib.to_s # equivalent to mkdir -p #{lib}
-    mkdir_p include.to_s
-    system "make", "all", "SYSTEM=gfortran", "MCFOST_INSTALL=#{HOMEBREW_PREFIX}", \
-           "MCFOST_NO_XGBOOST=yes", "MCFOST_GIT=1", "CXX=#{ENV.cxx}", \
-           "INCLUDE=-I#{HOMEBREW_PREFIX}/include", "LIBS=#{HOMEBREW_PREFIX}/lib"
-    bin.install "mcfost"
-    lib.install "libmcfost.a"
-    include.install "mcfost2phantom.mod"
+
+    Dir.chdir("src") do
+      system "make", "all", "SYSTEM=gfortran", "MCFOST_INSTALL=#{HOMEBREW_PREFIX}", \
+             "MCFOST_NO_XGBOOST=yes", "MCFOST_GIT=1", "CXX=#{ENV.cxx}", \
+             "INCLUDE=-I#{HOMEBREW_PREFIX}/include", "LIBS=#{HOMEBREW_PREFIX}/lib"
+      bin.install "mcfost"
+      lib.install "libmcfost.a"
+      include.install "mcfost2phantom.mod"
+    end
   end
 
   test do
-    system "false"
+    # Create a temporary directory for data files
+    utils_dir = "mcfost_utils"
+    Dir.mkdir(utils_dir) unless Dir.exist?(utils_dir)
+
+    # set MCFOST_UTILS
+    ENV["MCFOST_UTILS"] = File.expand_path(utils_dir)
+    ENV["MCFOST_GIT"] = "1"
+
+    # Run mcfost setup to download files to temp directory
+    system "#{bin}/mcfost", "-setup"
+
+    system "#{bin}/mcfost", "--help"
+  end
+
+  def caveats
+    <<~EOS
+      MCFOST requires the MCFOST_UTILS environment variable to be set for proper operation.
+
+      Add this to your shell profile (~/.zshrc, ~/.bash_profile, etc.):
+        export MCFOST_UTILS=~/mcfost_utils
+
+      Then download and install the data files as follows:
+        mcfost -setup
+
+      Check the MCFOST documentation for details.
+    EOS
   end
 end
