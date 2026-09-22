cb5b8fa189e61cb64be3f67e618a6aeff7143394
Daniel Price <daniel.price@monash.edu>
2026-09-22 10:37:19 +0000

    fits-previewer 0.8.0

--- Casks/fits-previewer.rb
+++ Casks/fits-previewer.rb
@@ -1,6 +1,6 @@
 cask "fits-previewer" do
-  version "0.7.1"
-  sha256 "bd6b22d6a1248c4c2ecfc05e5cde2da7e7e476bc36216decc525193b5df9de9e"
+  version "0.8.0"
+  sha256 "96d0f3eec8e0fc2cbbba332008deb2895950b22a2c3474600466e22c65d38b5e"
 
   url "https://github.com/danieljprice/fits-previewer/releases/download/v#{version}/FitsPreviewer.zip"
   name "FitsPreviewer"
